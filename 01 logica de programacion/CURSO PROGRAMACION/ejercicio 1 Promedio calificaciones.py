38e4976bb4b9dfb72af010098d330fdca85ab7c7
PilarSalas180223 <125762124+PilarSalas180223@users.noreply.github.com>
2023-03-04 16:47:29 -0600

    Add files via upload

---
+++ 01 logica de programacion/CURSO PROGRAMACION/ejercicio 1 Promedio calificaciones.py
@@ -0,0 +1,14 @@
+# prom de calif
+
+
+# ENTRADAS DE DATOS
+# Crear las variables
+dato_1 = float(input("Ingresa dato uno: "))
+dato_2 = float(input("Ingresa dato dos: "))
+dato_3 = float(input("Ingresa dato tres: "))
+
+# Procedimiento (numero y/o Lógicos)
+promedio = (dato_1 + dato_2 + dato_3) / 3
+
+# Salida de datos
+print(f"prom de calif = {round(promedio,2)}")
